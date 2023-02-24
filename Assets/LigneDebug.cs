b73d7d98330871199bb1ecdb358b9b84a476e0eb
Lemar Andar <lemarandar@pm.me>
2023-02-24 12:22:53 -0500

    tilemap, arrow and ennemy

--- Assets/LigneDebug.cs
+++ Assets/LigneDebug.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LigneDebug : MonoBehaviour
-{
+public class LigneDebug : MonoBehaviour{
     public Transform cible;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+
     }
 }
--- Assets/MiniRPG/Joueur.cs
+++ Assets/MiniRPG/Joueur.cs
@@ -5,24 +5,36 @@ using UnityEngine;
 public class Joueur : MonoBehaviour
 {
     private Rigidbody2D rig;
+    private Animator anim;
     private Vector2 mouvementfleche;
     public float vitesse;
+
+    [SerializeField]
+    private Transform prefabArrow;
 
     // Start is called before the first frame update
     void Start(){
         rig = GetComponent<Rigidbody2D>();
         mouvementfleche = Vector2.zero;
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update(){
         mouvementfleche.x = Input.GetAxisRaw("Horizontal");
         mouvementfleche.y = Input.GetAxisRaw("Vertical");
+        anim.SetFloat("Horizontal", mouvementfleche.x);
+        anim.SetFloat("Vertical", mouvementfleche.y);
+
+        if (Input.GetButtonDown("Jump")){
+            Instantiate(prefabArrow, transform.position, Quaternion.identity);
+        }
     }
 
     private void FixedUpdate(){
         rig.velocity = mouvementfleche * vitesse;
         float vel = rig.velocity.magnitude;
-        rig.velocity = rig.velocity.normalized / vel;
+        rig.velocity = rig.velocity.normalized * vitesse;
     }
 }
